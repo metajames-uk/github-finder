1d06ce79927e1aa235469a5b226be4d5f8afe887
James Hollands <james.hollands0190@gmail.com>
2019-12-17 21:19:51 +0000

    finished videos

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,32 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Alert from "./components/layout/Alert";
 import Navbar from "./components/layout/Navbar";
-import Spinner from "./components/layout/Spinner";
 import About from "./components/pages/About";
-import Search from "./components/users/Search";
+import Home from "./components/pages/Home";
 import User from "./components/users/User";
-import Users from "./components/users/Users";
-import GithubState from './context/github/GithubState';
-import AlertState from './context/alert/AlertState';
+import AlertState from "./context/alert/AlertState";
+import GithubState from "./context/github/GithubState";
+import NotFound from "./components/pages/notFound";
 const App = () => {
-
   return (
     <GithubState>
       <AlertState>
-    <Router>
-      <Fragment>
-        <Navbar />
-          <div className='container'>
-            <Alert></Alert>
-            <Switch>
-              <Route
-                exact
-                path='/'
-                render={props => (
-                  <Fragment>
-                    <Search />
-                    <Users />
-                  </Fragment>
-                )}
-              ></Route>
-              <Route exact path='/about'>
-                <About></About>
-              </Route>
-              <Route
-                exact
-                path='/user/:login'
-                component={User}
-              ></Route>
-            </Switch>
-          </div>
-      </Fragment>
-    </Router>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className='container'>
+              <Alert></Alert>
+              <Switch>
+                <Route exact path='/' component={Home}></Route>
+                <Route exact path='/about' component={About}></Route>
+                <Route exact path='/user/:login' component={User}></Route>
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
       </AlertState>
     </GithubState>
   );
--- src/context/github/githubReducer.tsx
+++ src/context/github/githubReducer.tsx
@@ -1,43 +1,42 @@
-import React from 'react';
 import {
-    SET_LOADING,
-    SEARCH_USERS,
-    CLEAR_USERS,
-    GET_USER,
-    GET_REPOS
-} from '../types'
+  CLEAR_USERS,
+  GET_REPOS,
+  GET_USER,
+  SEARCH_USERS,
+  SET_LOADING
+} from "../types";
 
-export default (state:any, action: any) => {
-    switch(action.type) {
-        case GET_REPOS:
-            return {
-                ...state,
-                repos: action.payload
-            }
-        case GET_USER:
-            return {
-                ...state,
-                user: action.payload,
-                loading: false
-            }
-        case CLEAR_USERS:
-            return {
-                ...state,
-                users: [],
-                loading: false
-            }
-        case SEARCH_USERS:
-            return {
-                ...state,
-                users: action.payload,
-                loading: false
-            }
-        case SET_LOADING:
-            return {
-                ...state,
-                loading: true
-            }
-        default:
-            return state
-    }
-}
+export default (state: any, action: any) => {
+  switch (action.type) {
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: action.payload
+      };
+    case GET_USER:
+      return {
+        ...state,
+        user: action.payload,
+        loading: false
+      };
+    case CLEAR_USERS:
+      return {
+        ...state,
+        users: [],
+        loading: false
+      };
+    case SEARCH_USERS:
+      return {
+        ...state,
+        users: action.payload,
+        loading: false
+      };
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    default:
+      return state;
+  }
+};
